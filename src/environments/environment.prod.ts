571d3e2ddb43d3fdbb21326c570abc8e56245cad
Tessa <104235420+TessaParker@users.noreply.github.com>
2024-04-16 14:58:57 -0700

    Changes for final launch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://htficocs7l.execute-api.us-east-1.amazonaws.com'
+  api_url: 'https://htficocs7l.execute-api.us-east-1.amazonaws.com/api'
 };
